8a9fb8f6255b172389351ecaaf196af2674b2ebb
rulo <schluet0r4life@gmail.com>
2024-10-04 17:41:27 +0200

    correct padding for overall layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MortgageResults from "./components/MortgageResults";
 function App() {
   return (
     <div className="bg-slate-100 w-full min-h-screen flex justify-center items-center">
-      <main className="w-full max-w-[1008px] grid md:p-10 lg:grid-cols-2">
+      <main className="w-full max-w-[1008px] grid lg:grid-cols-2">
         <MortgageCalculator />
         <div className="w-full bg-white lg:rounded-e-3xl">
           <div className="w-full h-full lg:flex lg:items-center lg:justify-center bg-slate-900 py-8 px-6 md:rounded-b-3xl lg:rounded-none lg:rounded-e-3xl lg:rounded-es-[80px] lg:p-10">
