d609621d781f815bf28e704074baad8ba89299ad
rulo <schluet0r4life@gmail.com>
2024-10-04 14:36:06 +0200

    transform clear all to button, adjust alignment and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <header className="flex flex-col gap-2 md:flex-row md:justify-between">
+    <header className="flex flex-col gap-2 md:flex-row md:justify-between md:items-baseline">
       <h1 className="text-xl text-slate-900 font-bold">Mortgage Calculator</h1>
-      <p className="text-slate-700 text-base underline underline-offset-2">
+      <button className="w-fit text-slate-700 hover:text-slate-900 transition-colors duration-300 text-base underline underline-offset-2">
         Clear All
-      </p>
+      </button>
     </header>
   );
 }
